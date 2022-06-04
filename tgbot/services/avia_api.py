2ad197a1a13aaf66402241f7977a4c35d47cd465
riser <riserlarenss@gmail.com>
2022-06-05 00:28:38 +0300

    Answer message

--- tgbot/services/avia_api.py
+++ tgbot/services/avia_api.py
@@ -26,6 +26,6 @@ class AviasalesAPI:
     def prices_for_dates(self, origin, destination, departure_at, return_at, unique, sorting, direct, currency, limit, page, one_way):
         args = self.get_str_latest_prices(origin=origin, destination=destination, departure_at=departure_at, return_at=return_at, one_way=one_way, sorting=sorting, unique=unique, direct=direct, currency=currency, limit=limit, page=page)
         response = requests.get("https://api.travelpayouts.com/aviasales/v3/prices_for_dates?"+args+f'token=74e374ffbb7e61ea2155743ac6ca4e64')
-        # return response.json()
-        return args
+        return response.json()
+        # return args
 
