7fbf9ff0c53d83507387b7616a7673257be56691
riser <riserlarenss@gmail.com>
2022-06-04 18:24:19 +0000

    Add insta aip

--- tgbot/services/instagramchik.py
+++ tgbot/services/instagramchik.py
@@ -3,21 +3,19 @@ from instagrapi.types import StoryMention, StoryMedia, StoryLink, StoryHashtag
 
 
 cl = Client()
-cl.login('Uchkuch', 'riser12345')
+cl.login('uchkuchh', 'riser12345')
+
 #
-# user_id = cl.user_id_from_username("adw0rd")
-# medias = cl.user_medias(int(user_id), 20)
+media_pk = cl.media_pk_from_url('https://www.instagram.com/p/CeYNTs8MHbg/')
+media_path = cl.video_download(media_pk)
+adw0rd = cl.user_info_by_username('dualipa')
+hashtag = cl.hashtag_info('dhbastards')
 #
-# media_pk = cl.media_pk_from_url('https://www.instagram.com/p/CGgDsi7JQdS/')
-# media_path = cl.video_download(media_pk)
-# adw0rd = cl.user_info_by_username('adw0rd')
-# hashtag = cl.hashtag_info('dhbastards')
-#
-# cl.video_upload_to_story(
-#     media_path,
-#     "Credits @adw0rd",
-#     mentions=[StoryMention(user=adw0rd, x=0.49892962, y=0.703125, width=0.8333333333333334, height=0.125)],
-#     links=[StoryLink(webUri='https://github.com/adw0rd/instagrapi')],
-#     hashtags=[StoryHashtag(hashtag=hashtag, x=0.23, y=0.32, width=0.5, height=0.22)],
-#     medias=[StoryMedia(media_pk=media_pk, x=0.5, y=0.5, width=0.6, height=0.8)]
-# )
+cl.video_upload_to_story(
+      media_path,
+     "Credits @adw0rd",
+    mentions=[StoryMention(user=adw0rd, x=0.49892962, y=0.703125, width=0.8333333333333334, height=0.125)],
+     links=[StoryLink(webUri='https://github.com/adw0rd/instagrapi')],
+     # hashtags=[StoryHashtag(hashtag=hashtag, x=0.23, y=0.32, width=0.5, height=0.22)],
+     medias=[StoryMedia(media_pk=media_pk, x=0.5, y=0.5, width=0.6, height=0.8)]
+)
